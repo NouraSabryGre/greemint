30e4f2792cf594bef234e80fed4116c033cee293
Mostafa Akram <mostafaakram@Mostafas-MacBook-Pro.local>
2017-05-18 01:41:13 +0200

    Seeding Doctor_user table

--- database/seeds/DoctorsTableSeeder.php
+++ database/seeds/DoctorsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DoctorsTableSeeder extends Seeder
 {
@@ -13,7 +14,16 @@ class DoctorsTableSeeder extends Seeder
     {
       factory(App\Doctor::class, 2)->create()->each(function ($d) {
         $d->save();
+        $user_id = factory(App\User::class)->create()->id;
+        $doctor_id = $d->id;
+        DB::table('doctor_user')->insert(
+          [
+            'user_id' => $user_id,
+            'doctor_id' => $doctor_id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+          ]
+        );
       });
-
     }
-}
+  }
