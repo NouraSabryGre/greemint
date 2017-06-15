feb5c6cdf69a167d52b5e43c2c6202310a663464
Mostafa Akram <mostafaakram@Mostafas-MacBook-Pro.local>
2017-06-15 17:49:42 +0200

    Hospital and Clinic --Places

--- app/Clinic.php
+++ app/Clinic.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Clinic extends Place
 {
     protected $table = 'clinics';
+
+    /**
+    * Get the uer record associated with the doctor.
+    */
+    public function place()
+    {
+        return $this->belongsTo('App\Place');
+    }
+
 }
--- app/Hospital.php
+++ app/Hospital.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Hospital extends Place
 {
     protected $table = 'hospitals';
+    /**
+    * Get the uer record associated with the doctor.
+    */
+    public function place()
+    {
+        return $this->belongsTo('App\Place');
+    }
+
 }
