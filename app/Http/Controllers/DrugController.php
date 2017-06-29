7eb4a35aecfb0a4d37a1bbcad1efa20a7cf45171
Mostafa Akram <mostafaakram@Mostafas-MacBook-Pro.local>
2017-06-29 13:50:54 +0200

    Search Drugs in Controller

--- app/Http/Controllers/DrugController.php
+++ app/Http/Controllers/DrugController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Drug;
 class DrugController extends Controller
 {
-    //
+    
+    public function search($key = null)
+    {
+      $drugs = Drug::where('name', 'LIKE', '%'.$key.'%')->get();
+      $drugsNames = array();
+      foreach ($drugs as $drug)
+      {
+        $drugsNames[]['name'] = $drug->name;
+      }
+      return json_encode($drugsNames);
+    }
 }
